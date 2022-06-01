01a0a94cda17885ca9083941c96f6a77526f2ce1
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-01 16:28:06 -0300

    chore: addon auth message and other configs

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  
+  before_action :authenticate_user!
+
   def not_found
     render body: nil, :status => 404
   end
--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_153418) do
+ActiveRecord::Schema.define(version: 2022_06_01_185951) do
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "artist_id", null: false
+    t.string "name", null: false
+    t.integer "year", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -31,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_06_01_153418) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
