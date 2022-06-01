e384330a309291fb8483556701dbedc5902be7a4
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-01 12:32:43 -0300

    feat(gems): addon sidekiq and rspec

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,3 +1,10 @@
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
+  match "*path" => "application#not_found", via: :all
 end
