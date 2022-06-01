8bc7b7ebab0c3bb95209eb1e7c0bf2f9ce4d1736
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-01 12:43:29 -0300

    feat(gem): devise and DTA

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
   def not_found
     render body: nil, :status => 404
   end
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -4,7 +4,8 @@ require 'sidekiq/web'
 Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
+  mount_devise_token_auth_for 'User', at: 'auth'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match "*path" => "application#not_found", via: :all
 end
