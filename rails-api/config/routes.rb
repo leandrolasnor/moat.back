05d8e477c477db87fcf18072eaad03b7626ec381
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-03 01:02:17 -0300

    feat: routes for album index and /cable

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -5,8 +5,9 @@ Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  mount ActionCable.server => "/cable"
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :albums
+  post '/albums', to: 'albums#index'
   match "*path" => "application#not_found", via: :all
 end
