d1887a6d930ab511625a1139ac11cfff945711a3
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-03 17:53:58 -0300

    test: signing with admin user

--- rails-api/spec/support/auth/auth_helper.rb
+++ rails-api/spec/support/auth/auth_helper.rb
@@ -1,7 +1,7 @@
 module Auth
-  def get_headers
+  def get_headers(admin: 0)
     user = User.where(uid: 'teste@teste.com')
-    user = User.create!(email: 'teste@teste.com',password: '123456',password_confirmation: '123456') if user.blank?
+    user = User.create!(email: 'teste@teste.com',password: '123456',password_confirmation: '123456', role: admin) if user.blank?
     sign_in_header = {"Content-Type": "application/json"}
     post user_session_path, params: {:email => "teste@teste.com", :password => "123456"}.to_json, headers: sign_in_header
     headers = {
