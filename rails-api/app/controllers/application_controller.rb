612b3a6bfad9c7a074b5f6b626f6f8f25d99c684
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-03 00:59:05 -0300

    chore: devise config

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
-  before_action :authenticate_user!
-
   def not_found
     render body: nil, :status => 404
   end
+
+  protected
+
+    def configure_permitted_parameters
+        added_attrs = [:email, :last_name, :name, :role, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 end
