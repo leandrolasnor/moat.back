686e03940a58ae0f3be5fb9147dcfaceb0817c5a
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-03 00:59:28 -0300

    feat: album model

--- rails-api/app/models/album.rb
+++ rails-api/app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ApplicationRecord
   validates :artist_id, :name, presence: true
-  validates :year, numericality: { only_integer: true, greater_than: 1947 }, presence: true
+  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1948 }, presence: true
+
+  def artist
+    @artist ||= Moat::API::Task::Artist::Find.call(self.artist_id)
+  end
 end
