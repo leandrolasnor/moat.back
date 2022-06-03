2dbd0e2cb54e7386133f9f7960ea89e54ac8af89
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-03 12:31:22 -0300

    fix: names change

--- rails-api/app/controllers/albums_controller.rb
+++ rails-api/app/controllers/albums_controller.rb
@@ -1,7 +1,7 @@
 class AlbumsController < ApiController
   before_action :set_album, only: [:show]
 
-  def index
+  def search
     SearchAlbumsService.call album_search_params
   end
 
@@ -10,7 +10,7 @@ class AlbumsController < ApiController
   end
 
   def create
-    CreateAlbumsService.call album_create_params
+    CreateAlbumService.call album_create_params
   end
 
   def update
@@ -25,6 +25,11 @@ class AlbumsController < ApiController
 
     def set_album
       @album = Album.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render body: nil, :status => 404
+    rescue => e
+      Rails.logger.error e.inspect
+      render body: nil, :status => 500
     end
 
     def album_params
@@ -36,11 +41,16 @@ class AlbumsController < ApiController
     end
 
     def album_destroy_params
-      album_params.permit(:id, :uid)
+      album_params.merge(
+        {
+          id: params[:id], 
+          channel:current_user.to_gid_param
+        }
+      ).permit(:id, :uid)
     end
 
     def album_update_params
-      album_params.permit(:id, :name,:year,:artist_id, :uid)
+      album_params.merge({id: params[:id]}).permit(:id, :name,:year,:artist_id, :uid)
     end
 
     def album_search_params
