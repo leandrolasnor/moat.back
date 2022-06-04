fc9425bd9650dfcfacd939b5321995031a9efae5
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-04 17:58:27 -0300

    chore:  staging env with postgresql on heroku

--- rails-api/config/environments/staging.rb
+++ rails-api/config/environments/staging.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.hosts << ENV.fetch('HOST') { "moat-task.herokuapp.com" }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
