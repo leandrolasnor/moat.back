a53b2649d97088b955c6ff7f8efe33746175480a
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-04 09:09:07 -0300

    chore: changing place routes

--- rails-api/config/environments/development.rb
+++ rails-api/config/environments/development.rb
@@ -42,6 +42,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # ActionCable Route
+  config.action_cable.mount_path = '/cable'
+
+  # ActionCable whitelist
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, nil]
+
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -5,10 +5,10 @@ Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  mount ActionCable.server => "/cable"
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   get  '/albums/search', to: 'albums#search'
   resources :albums, id: /[a-z0-9\-_]+/, only: [:show, :update, :destroy, :create]
+
   match "*path" => "application#not_found", via: :all
 end
