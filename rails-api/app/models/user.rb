eaea9f5788905e1f1dc8ec2bd3ae616ededcb4c2
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-07 23:15:00 -0300

    feat: handle invalid role enum with liberal enum

--- rails-api/app/models/user.rb
+++ rails-api/app/models/user.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  include LiberalEnum
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   self.primary_key = 'uid'
-  enum role: [:user, :admin]
+  enum role: { user: 0, admin: 1 }
+  liberal_enum :role
+
+  validates :name, presence: true
+  validates :role, presence: true, inclusion: { in: roles.values }
+  validates_numericality_of :role, on: :create, message: "is not a number"
+  validates :password, presence: true, on: :create
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, if: lambda {| u| u.password.present? }
 end
