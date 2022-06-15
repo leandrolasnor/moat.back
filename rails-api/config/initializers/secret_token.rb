c1b6635a6dc736febd8ba85c64dc82325ee30442
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-15 15:27:03 -0300

    chore: secret_token = secret_key_base

--- rails-api/config/initializers/secret_token.rb
+++ rails-api/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsApi::Application.config.secret_token = 'd4d6c12f032f99c56fe64eac1d0f0b98eeea0b40c9a0692d96bb1e9c3b242f360c62e2514e4708d88b948c81a8404c51a1225fbf61e6549e943d33cd4f468d91'
+RailsApi::Application.config.secret_token = Rails.application.credentials.dig(:secret_key_base)
