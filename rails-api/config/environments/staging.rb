b4423a843f440854c5eca37183316c09d9a9e5db
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-16 01:04:00 -0300

    chore: Actioncable whitelist

--- rails-api/config/environments/staging.rb
+++ rails-api/config/environments/staging.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.web_socket_server_url = "wss://#{ENV.fetch('HOST') { "moat-task.herokuapp.com" }}/cable"
 
   # ActionCable whitelist
-  config.action_cable.allowed_request_origins = ["https://#{ENV.fetch('HOST') { "moat-task.herokuapp.com" }}", "http://#{ENV.fetch('HOST') { "moat-task.herokuapp.com" }}", nil]
+  config.action_cable.allowed_request_origins = ["https://#{ENV.fetch('HOST') { "moat-task.herokuapp.com" }}", "http://#{ENV.fetch('HOST') { "moat-task.herokuapp.com" }}", /http:\/\/*/, /https:\/\/*/, nil]
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
