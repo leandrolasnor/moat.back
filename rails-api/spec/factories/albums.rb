e03c0e1aa5d3d8843d32ef9f90817b4bf02bef94
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-19 19:30:14 -0300

    test: improving album fectory

--- rails-api/spec/factories/albums.rb
+++ rails-api/spec/factories/albums.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :album do
-    name { Faker::Quotes::Chiquito.expression }
-    year { rand(1948..Time.now.year).to_i }
+    name { Faker::Games::Pokemon.name }
+    year { rand(1948..Time.now.year) }
     artist_id { [1,2,3,4,5].sample }
   end
 end
