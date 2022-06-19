8b6b3ad328e63de7995381d5b5aaf5620694d189
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-19 19:30:57 -0300

    test: routing

--- rails-api/spec/routing/albums_routing_spec.rb
+++ rails-api/spec/routing/albums_routing_spec.rb
@@ -3,28 +3,36 @@ require "rails_helper"
 RSpec.describe AlbumsController, type: :routing do
   describe "routing" do
 
-    it "routes to #search" do
+    it "route to #search" do
       expect(get: "/albums/search").to route_to("albums#search")
     end
 
-    it "routes to #show" do
+    it "route to #show" do
       expect(get: "/albums/1").to route_to("albums#show", id: "1")
     end
 
-    it "routes to #create" do
+    it "route to #create" do
       expect(post: "/albums").to route_to("albums#create")
     end
 
-    it "routes to #update via PUT" do
+    it "route to #update via PUT" do
       expect(put: "/albums/1").to route_to("albums#update", id: "1")
     end
 
-    it "routes to #update via PATCH" do
+    it "route to #update via PATCH" do
       expect(patch: "/albums/1").to route_to("albums#update", id: "1")
     end
 
-    it "routes to #destroy" do
+    it "route to #destroy" do
       expect(delete: "/albums/1").to route_to("albums#destroy", id: "1")
+    end
+
+    describe "route to #show" do
+      context "without id param" do
+        it "should trigger not found" do
+          expect(get: "/albums").to route_to("api#not_found", path: 'albums')
+        end
+      end
     end
   end
 end
