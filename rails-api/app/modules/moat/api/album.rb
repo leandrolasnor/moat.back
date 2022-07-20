d70c23d3e9c310b18fc345cca9d28f9bb404cb34
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-07-20 01:55:55 -0300

    refactor: external api left us

--- rails-api/app/modules/moat/api/album.rb
+++ rails-api/app/modules/moat/api/album.rb
@@ -12,10 +12,17 @@ module Moat::Api
 
     private
     def get_artist(id)
-      response = HTTParty.get("#{uri}?artist_id=#{id}", headers: headers)
-      return JSON.parse(response.body, symbolize_names: true).first if response.code == 200
-      Rails.logger.error({ from: "Moat::Artist.find(#{id})", code: response.code, body: response.body })
-      {}
+
+      # response = HTTParty.get("#{uri}?artist_id=#{id}", headers: headers)
+      # return JSON.parse(response.body, symbolize_names: true).first if response.code == 200
+      # Rails.logger.error({ from: "Moat::Artist.find(#{id})", code: response.code, body: response.body })
+      [
+        {id: 1, name: 'Ed Sheeran', twitter: '@edsheeran'},
+        {id: 2, name: 'Eminem', twitter: '@Eminem'},
+        {id: 3, name: 'Maroon 5', twitter: '@maroon5'},
+        {id: 4, name: 'Coldplay', twitter: '@coldplay'},
+        {id: 5, name: 'Bruno Mars', twitter: '@BrunoMars'}
+      ].select {|artist| artist[:id] == id }.last
     end
 
     def uri
